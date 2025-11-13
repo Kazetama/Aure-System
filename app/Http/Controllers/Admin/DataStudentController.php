45360954d8575faf3e0e6d2cb11c892b03ac6405
kazetama <tamaketuahimpunan@gmail.com>
2025-11-13 13:56:02 +0700

    refactor: reorganize UpdateStudentRequest and update namespaces in DataStudentController

--- app/Http/Controllers/Admin/DataStudentController.php
+++ app/Http/Controllers/Admin/DataStudentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
-use App\Http\Requests\UpdateStudentRequest;
+use App\Http\Requests\Students\UpdateStudentRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -26,7 +26,7 @@ class DataStudentController extends Controller
 
         $students = $query->orderBy('name', 'asc')->paginate(10)->withQueryString();
 
-        return Inertia::render('Admin/Students/Index', [
+        return Inertia::render('admin/students/index', [
             'students' => $students,
             'filters' => $request->only('search'),
         ]);
@@ -35,7 +35,7 @@ class DataStudentController extends Controller
     // Halaman edit data siswa
     public function edit(Student $student)
     {
-        return Inertia::render('Admin/Students/Edit', [
+        return Inertia::render('admin/students/edit', [
             'student' => $student,
         ]);
     }
